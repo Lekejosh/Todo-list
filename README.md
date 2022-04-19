1f417a3d724e02f37bbf59c1dd7c5f43ddc6762a
lekejosh <lekejosh6wf@gmail.com>
2022-04-19 12:34:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TodoList-App
+A todo list web application built with plain javascript.
+
+Live link : https://lakesxo.github.io/TodoList-App/
---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+// Needed elements from Html
+let darkModeBtn = document.getElementById("dark-mode");
+let userInput = document.querySelector("input");
+let addTodoBtn = document.getElementById("addTodo");
+let body = document.querySelector("body");
+let h1 = document.querySelector("h1");
+let welcomeText = document.querySelector("p");
+let ul = document.querySelector("ul.todos");
+
+// Create delete Todo button at the end of every Todo list
+let myTodolist = document.getElementsByTagName("LI");
+for (i = 0; i < myTodolist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myTodolist[i].appendChild(span);
+}
+
+// Create each todo and append to the parent element
+attachTodo = () => {
+  let list = document.createElement("li");
+  list.appendChild(document.createTextNode(userInput.value));
+  ul.appendChild(list);
+  userInput.value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  list.appendChild(span);
+
+  let close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+};
+
+// Dark mode function
+darkMode = () => {
+  body.classList.toggle("dark-mode");
+  h1.classList.toggle("lol");
+  userInput.classList.toggle("user");
+  welcomeText.classList.toggle("lol");
+  addTodoBtn.classList.toggle("user2");
+};
+
+// Create a key in localStorage for theme
+
+// Create todo only when an input is entered
+addTodo = () => {
+  if (userInput.value.length > 0) {
+    attachTodo();
+  }
+};
+
+// Create todo when an input is entered and the enter key is pressed
+enterKeyAddTodo = (e) => {
+  if (userInput.value.length > 0 && event.which === 13) {
+    attachTodo();
+  }
+};
+
+// Event listeners
+darkModeBtn.addEventListener("click", darkMode);
+
+addTodoBtn.addEventListener("click", addTodo);
+userInput.addEventListener("keypress", enterKeyAddTodo);
